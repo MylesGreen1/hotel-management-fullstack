5968d1e2d9358fc32d3bdf895845336f98d73533
Myles Green <mgr1319@wgu.edu>
2026-09-08 15:48:56 -0400

    Add logging for multilingual welcome messages

--- src/main/java/edu/wgu/d387_sample_code/rest/WelcomeController.java
+++ src/main/java/edu/wgu/d387_sample_code/rest/WelcomeController.java
@@ -9,11 +9,16 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 import java.util.Locale;
 import java.util.concurrent.CompletableFuture;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @RestController
 @RequestMapping("/api")
 @CrossOrigin
 public class WelcomeController {
+
+    private static final Logger logger =
+            LoggerFactory.getLogger(WelcomeController.class);
 
     private final WelcomeMessageService welcomeMessageService;
 
@@ -23,11 +28,16 @@ public class WelcomeController {
 
     @GetMapping("/welcome")
     public List<String> getWelcomeMessages() {
+
+        logger.info("Received request for localized welcome messages");
+
         CompletableFuture<String> english =
                 welcomeMessageService.getWelcomeMessage(Locale.US);
 
         CompletableFuture<String> french =
                 welcomeMessageService.getWelcomeMessage(Locale.CANADA_FRENCH);
+
+        logger.info("Successfully generated English and French welcome messages");
 
         return List.of(english.join(), french.join());
     }
